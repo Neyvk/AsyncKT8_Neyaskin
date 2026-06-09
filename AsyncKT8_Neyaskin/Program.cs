5adbd665e66463fd54143611259e1b58a1a16898
bebe <neyaskin.kv@gmail.com>
2026-06-09 23:25:44 +0300

    Add project files.

---
+++ AsyncKT8_Neyaskin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reactive.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        var randomNumbers = Observable
+            .Interval(TimeSpan.FromMilliseconds(500))
+            .Select(_ => random.Next(1, 101))
+            .Where(x => x > 50)
+            .TakeUntil(
+                Observable.Timer(TimeSpan.FromSeconds(10))
+            );
+
+        randomNumbers.Subscribe(
+            x => Console.WriteLine($"Received number: {x}"),
+            ex => Console.WriteLine($"Error: {ex.Message}"),
+            () => Console.WriteLine("Stream completed")
+        );
+
+        Console.ReadLine();
+    }
+}
